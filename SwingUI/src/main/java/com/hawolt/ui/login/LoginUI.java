d46b530e439409ecea0d0d20a55d6bccfe58224b
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2023-08-19 01:33:40 +0100

    Update LoginUI.java

--- SwingUI/src/main/java/com/hawolt/ui/login/LoginUI.java
+++ SwingUI/src/main/java/com/hawolt/ui/login/LoginUI.java
@@ -86,9 +86,9 @@ public class LoginUI extends MainUIComponent implements ActionListener {
             login.getModel().setPressed(false);
             login.update(login.getGraphics());
             Logger.error(e);
-            // DevNote: Current flow is to recover the League client by calling Logger.fatal. 
-            // Removing this line will cause the login flow to re-try its priot state which may-
-            // result in you being rate limited.
+            // Why: Flow to recover the League login client is calling Logger.fatal(e). 
+            // Removing this line will cause the login flow to re-try its prior state e.g. authorization endlessly.
+            // This may result in you being rate limited.
             Logger.fatal(e);
         }
     }
